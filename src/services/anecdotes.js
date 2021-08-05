d61beb26a4ad1609beeeafc28a64e3d2c34f9601
Pawel Cebula <ptcebula@gmail.com>
2021-08-05 22:56:43 +0200

    fix: change baseUrl to relative path

--- src/services/anecdotes.js
+++ src/services/anecdotes.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseUrl = 'http://localhost:3001/db/anecdotes'
+const baseUrl = '/db/anecdotes'
 
 const getAll = async () => {
   const response = await axios.get(baseUrl)
